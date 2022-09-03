17264073d622f0b005e421fbe2bc020d5058ccf1
fbraz83 <104683510+fbraz83@users.noreply.github.com>
2022-09-03 13:57:32 -0300

    Criação da pagina suco de laranja

--- PersonalCheffProj/receitas/views.py
+++ PersonalCheffProj/receitas/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'index.html')
 
+def sucodelaranja(request):
+    return render(request, 'sucodelaranja.html')
+
 
